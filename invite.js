db0b15f9b1dedbeb9aaf77683bf8c214505f623f
David Malaschonok <davidmalaschonok@gmail.com>
2017-04-09 21:17:04 +0200

    Almost made bot distinguish the groups.

--- invite.js
+++ invite.js
@@ -1,9 +1,23 @@
 module.exports = function (msg) {
-    this.sendMessage(msg.chat.id, this.constants.invitationText).catch(
-        function (error) {
-            // bot was probably removed
-            console.log(chalk.red(logSymbols.error, error));
-            return;
+    for (group in this.constants.invitationText) {
+        userInGroup = false;
+        for (message in this.messageLog.messages)
+        {
+            console.log(message.from.id)
+            console.log(msg.from.id)
+            console.log(message.chat.title)
+            console.log(group)
+            console.log()
+            if (message.from.id == msg.from.id && message.chat.title == group)
+                userInGroup = true;
         }
-    );
+        if (userInGroup)
+            this.sendMessage(msg.chat.id, this.constants.invitationText[group]).catch(
+                function (error) {
+                    // bot was probably removed
+                    console.log(chalk.red(logSymbols.error, error));
+                    return;
+                }
+            );
+    }
 }
